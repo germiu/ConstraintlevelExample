f9e571af1ee36e973bc1f51b4848c2578c43fb16
DanJuma <thelordmessanger@protonmail.com>
2022-09-23 17:09:40 +0400

    first loginAuth

--- app/src/main/java/com/example/constraint/MainActivity.java
+++ app/src/main/java/com/example/constraint/MainActivity.java
@@ -2,13 +2,52 @@ package com.example.constraint;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    String userName = "Isah";
+    String password = "Musa";
+
+    EditText editTextTextEmailAddress,editTextTextPassword2;
+
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editTextTextEmailAddress = findViewById(R.id.editTextTextEmailAddress);
+        editTextTextPassword2 = findViewById(R.id.editTextTextPassword2);
+        button=findViewById(R.id.button);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String use = editTextTextEmailAddress.getText().toString();
+                String pass = editTextTextPassword2.getText().toString();
+
+
+                if(use.equals(userName) && pass.equals(password)){
+
+                    Intent i = new Intent(MainActivity.this,LoginActivity.class);
+                    startActivity(i);
+
+                }
+                else {
+                    Toast.makeText(MainActivity.this, "Please enter correct password", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
 }
